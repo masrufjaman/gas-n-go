583d4ad48bcfdc7b78a076b77027abb29fc37afa
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-03-21 19:48:08 +0600

    Add views for home, about and help

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About Us</h1>')
+
+
+def help(request):
+    return HttpResponse('<h1>Help</h1>')
