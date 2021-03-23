d6135fbc95e2d4691cab6f0db403e14e749cfbe7
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-03-23 12:26:33 +0600

    Changed URL name

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home-page'),
-    path('about/', views.about, name='about'),
-    path('help/', views.help, name='help'),
+    path('', views.home, name='home-home'),
+    path('about/', views.about, name='home-about'),
+    path('help/', views.help, name='home-help'),
 ]
