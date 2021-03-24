0615856013d974011d21f3c90d276f2971e51638
Anondo Hossain Rafi <hossain.rafi@northsouth.edu>
2021-03-24 22:43:44 +0600

    Customer Table added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Customer (models.Model):
+    Customer_Name=models.CharField(max_length=100)
+    Customer_ID=models.CharField(max_length=100)
+    Customer_Contact=models.CharField(max_length=30)
+    Customer_License=models.CharField(max_length=100)
+    Customer_Mail=models.CharField(max_length=100)
