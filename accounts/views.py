4336e52b4b459b1eb97ffe62547a051d58f0342b
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-03-27 14:00:01 +0600

    Updated static files link

--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,4 @@ from django.contrib import messages
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('home-home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'title': 'Register'})
