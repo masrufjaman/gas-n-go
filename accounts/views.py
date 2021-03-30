02439e547aff0f7e02930b6246aac8993321f40f
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-03-31 00:57:38 +0600

    registration

--- accounts/views.py
+++ accounts/views.py
@@ -29,4 +29,24 @@ def register(request):
 
 
 def login(request):
-    return render(request, 'accounts/login.html', {'title': 'Login'})
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(
+                request, f'Username "{username}" already taken...')
+            return redirect('accounts-register')
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, f'Email "{email}" already taken...')
+            return redirect('accounts-register')
+        else:
+            user = User.objects.create_user(
+                username=username, password=password, email=email)
+            user.save()
+            messages.success(request, f'Account created for {username}!')
+        return redirect('home-home')
+
+    else:
+        return render(request, 'accounts/login.html', {'title': 'Login'})
