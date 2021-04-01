ccd99e07fefba946e332f0d26db502634ecf095b
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-04-01 15:21:52 +0600

    added services urls and views

--- services/views.py
+++ services/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def service(request):
+    return render(request, 'services/service.html', {'title': 'Service'})
