3dfff09f5ec368694cc198d98ce592aab4af1776
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-04-05 23:20:03 +0600

    added eStore app

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eStore.apps.EstoreConfig',
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
