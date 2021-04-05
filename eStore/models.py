40693aaf17c8f6b9bcfc2bf38cd3be6fed24e9ed
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-04-05 23:41:10 +0600

    Items, OrderItems & Order models

--- eStore/models.py
+++ eStore/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from accounts.models import *
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item
+
+
+class Order(models.Model):
+    username = models.ForeignKey(
+        Customer, to_field='username', on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
