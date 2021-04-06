a7dfd114d9d81d8c0109e5f384210e7338df561c
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-04-06 14:03:05 +0600

    link to home and migrate

--- eStore/views.py
+++ eStore/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 from .models import *
 
+
 def item_list(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, "item_list.html", context)
+    return render(request, "eStore/item_list.html", context)
