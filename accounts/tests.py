96d481a2a9016d1907f67fbfd71d68f9c98927f0
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-05-28 08:32:25 +0600

    Update tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import SESSION_KEY
+from .models import Customer
 from django.test import TestCase
 
-# Create your tests here.
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        Customer.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        # login
+        response = self.client.post('/login/', **self.credentials)
+        # should be logged in now, fails however
+        self.assertTrue(response.context['user'].is_active)
